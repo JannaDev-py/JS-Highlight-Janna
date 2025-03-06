d0a7c95ff18c971e113d4b8dcb1111a91961e843
JannaDev-py <orlandojannael@gmail.com>
2025-03-06 11:37:47 -0600

    advance: making editable highlight, but theres a problem it can make enter

--- script.js
+++ script.js
@@ -18,21 +18,42 @@ document.addEventListener("DOMContentLoaded", ()=>{ updateCodeHighlight()});
 
 function updateCodeHighlight(){
     const code = document.querySelectorAll(".code"); //make an node list for all the code elements
-    const symbolRegex = /(?<!<[^>]*)[;=+\-{}\[\]\(\)$]|(&gt;)|(&lt;)/g;   // symbols "><" there no like that in the code are &gt; and &lt;
-    const stringRegex = /["'`](.*?)["'`]/g;
-    const commentRegex = /\/\/.*/g;
 
     if(code){
         code.forEach(function(el){
-            //get all codes and highlight them
-            const stringHTMLCodeHighlight = simpleHighlight(el.innerHTML, stringRegex, "string");
-            const keywordHTMLCodeHighlight = keywordHighlight(stringHTMLCodeHighlight);
-            const methodHTMLCodeHighlight = methodHighlight(keywordHTMLCodeHighlight);
-            const symbolHTMLCodeHighlight = simpleHighlight(methodHTMLCodeHighlight, symbolRegex, "symbol");
-            const commentHTMLCodeHighlight = simpleHighlight(symbolHTMLCodeHighlight, commentRegex, "comment");
-            el.innerHTML = commentHTMLCodeHighlight; 
-        })
+            if(el.getAttribute("contenteditable")){
+                //if user put contenteditable on the code, then we need make the content highlighted editable
+                //then we need to make a div with the content of the element and highlight it
+                el.appendChild(document.createElement("DIV"));
+                const div = el.querySelector("div");
+                div.innerHTML = el.textContent;
+                div.innerHTML = setHighlight(div.textContent);
+                
+                //what is being editable is the element that has the contenteditable attribute and we get that content constantly to put it highlighted on the div that show the code, because it works with a div that be behind the element and the principal element it is in front but with content color transparent, per shows only the div with the highlighted code
+                el.addEventListener("input", ()=>{
+                    const contentRegex =/((?:[^<]|<(?!div\b[^>]*>))*)/;
+                    const contentWithoutEditableDiv = el.innerHTML.match(contentRegex)[0];  
+                    const contentHighlighted = setHighlight(contentWithoutEditableDiv);
+                    div.innerHTML = contentHighlighted;
+                })        
+            }else {
+                el.innerHTML = setHighlight(el.textContent);
+                el.style.color = "var(--based-color-text)";
+            }
+        });
     }
+}
+function setHighlight(el){
+    const symbolRegex = /(?<!<[^>]*)[;=+\-{}\[\]\(\)$]|(&gt;)|(&lt;)/g;   // symbols "><" there no like that in the code are &gt; and &lt;
+    const stringRegex = /["'`](.*?)["'`]/g;
+    const commentRegex = /\/\/.*/g;        
+    //get all codes and highlight them
+    const stringHTMLCodeHighlight = simpleHighlight(el, stringRegex, "string");
+    const keywordHTMLCodeHighlight = keywordHighlight(stringHTMLCodeHighlight);
+    const methodHTMLCodeHighlight = methodHighlight(keywordHTMLCodeHighlight);
+    const symbolHTMLCodeHighlight = simpleHighlight(methodHTMLCodeHighlight, symbolRegex, "symbol");
+    const commentHTMLCodeHighlight = simpleHighlight(symbolHTMLCodeHighlight, commentRegex, "comment");
+    return commentHTMLCodeHighlight;
 }
 
 //make functions that highlight the code by sections, per can search for the changes on each section
